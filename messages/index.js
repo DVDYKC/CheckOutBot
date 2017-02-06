c7ee681595421efa2a4f05f927b083eb2b74f003
Yip <David.Yip@MarinaBaySands.com>
2017-02-06 17:56:28 +0800

    luis

--- messages/index.js
+++ messages/index.js
@@ -32,6 +32,57 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
+.matches('Checkouttime',[
+    function (session, args, next) {
+        session.send('The check out time is 11 am');
+    }    
+])
+.matches('Thanks',[
+    function (session, args, next) {
+        session.send('You’re welcome.');
+    }    
+])
+.matches('StartConvo',[
+    function (session, args, next) {
+        session.send('How may I help you?');
+    }    
+])
+.matches('AirportCab',[
+    function (session) {
+        builder.Prompts.confirm(
+            session,
+            'Do you want me to call a cab to the airport',
+            {
+                listStyle: builder.ListStyle.button
+            });
+    },
+    function (session, results) {
+        if (results.response) {
+            session.send('Please proceed to the lobby. Our bell boy will call a cab for you.');
+            return session.endDialog();
+        } else {
+            //return session.beginDialog('withoutroomreservations');
+        }
+    }   
+])
+.matches('checkout',[
+    function (session) {
+        builder.Prompts.confirm(
+            session,
+            'Do you also wish to do express check out now?',
+            {
+                listStyle: builder.ListStyle.button
+            });
+    },
+    function (session, results) {
+        if (results.response) {
+            session.send('Please proceed to the lobby. Our bell boy will call a cab for you.');
+            return session.endDialog();
+        } else {
+            //return session.beginDialog('withoutroomreservations');
+        }
+    }   
+])
 .matches('None', (session, args) => {
     session.send('Hi! This is the None intent handler. You said: \'%s\'.', session.message.text);
 })
